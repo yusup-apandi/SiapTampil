b3009096a87fa805438526f32684e37ebb2c57ad
Yusup Apandi <51903213+yusup-apandi@users.noreply.github.com>
2023-09-19 22:03:23 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./*.html','./**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary': {
+          '50': '#E6E7F0',
+          '100': '#B1B5D1',
+          '200': '#8B91BB',
+          '300': '#565E9C',
+          '400': '#353F89',
+          '500': '#030F6B',
+          '600': '#030E61',
+          '700': '#020B4C',
+          '800': '#02083B',
+          '900': '#01062D',
+        }
+        },
+    },
   },
   plugins: [],
 }
